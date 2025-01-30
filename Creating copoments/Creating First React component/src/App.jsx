91114394645db188a6fb57afcd8036f746b7d401
Your Name <you@example.com>
2025-01-30 16:46:55 +0500

    components done

---
+++ Creating copoments/Creating First React component/src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import {One} from "./one"
+ function App() {
+
+  return (
+    <One/>
+  )
+}
+
+export default App
---
+++ Creating copoments/Creating First React component/src/one.jsx
@@ -0,0 +1,5 @@
+export function One(){
+    return <>
+    <h1>Hello world of compoents</h1>
+    </>
+}
