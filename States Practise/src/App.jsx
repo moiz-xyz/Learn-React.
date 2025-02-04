46c79212bae1d5cce63b6c4d53fdc98a6c8071a6
Your Name <you@example.com>
2025-02-04 23:47:47 +0500

    pending

---
+++ States Practise/src/App.jsx
@@ -0,0 +1,14 @@
+import React,{useState} from "react"
+function App(){
+
+  const [visible , show] = useState(false);
+  const [unVisible , hide] = useState(true);
+
+  return  <>
+ 
+ <input type="text" name="" id=""  placeholder="Enter any thing"/>
+ {/* <button onClick={ () =>(){}}> Show text</button> */}
+  </>
+}
+
+export default App
