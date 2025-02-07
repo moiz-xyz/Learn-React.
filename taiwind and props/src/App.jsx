82d306c8b434c4af4b0aaf109053ecbddbaef90b
Your Name <you@example.com>
2025-02-07 23:27:36 +0500

    tailwind done

---
+++ taiwind and props/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <span class="box-decoration-clone bg-linear-to-r from-indigo-600 to-pink-500 px-2 text-white ...">
+  Hello<br />World
+</span>
+    </>
+  )
+}
+
+export default App
